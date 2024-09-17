39ac0f42dff6b43d1637ab82d990786973e78c78
luwat <oluwatobioreniyi@gmail.com>
2024-09-17 17:35:43 +0100

    restyle tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,29 +5,44 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 const Layout = () => {
   return (
     <>
-      <Tabs>
+      <Tabs screenOptions={{
+        tabBarStyle: {
+          height: 70,
+          backgroundColor: "#161616",
+        },
+        tabBarActiveBackgroundColor: "#161616",
+        tabBarActiveTintColor: "#fafafa",
+        tabBarLabelStyle: {
+          fontSize: 16,
+          paddingBottom: 5
+        },
+        tabBarInactiveTintColor: "grey",
+        
+      }}>
         <Tabs.Screen
           name="employees"
           options={{
             title: "Employees",
+            headerShown: false,
             tabBarIcon: ({ focused }) => (
               <Ionicons
                 name={focused ? "person-circle" : "person-circle-outline"}
-                size={28}
-                color={focused ? "black" : "gray"}
+                size={32}
+                color={focused ? "#fafafa" : "gray"}
               />
-            ),
+            )
           }}
         />
         <Tabs.Screen
           name="create"
           options={{
             title: "Create",
+            headerShown: false,
             tabBarIcon: ({ focused }) => (
               <Ionicons
                 name={focused ? "add-circle" : "add-circle-outline"}
-                size={28}
-                color={focused ? "black" : "gray"}
+                size={32}
+                color={focused ? "#fafafa" : "gray"}
               />
             ),
           }}
