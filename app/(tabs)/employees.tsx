9d46fedf0528b053a9dd365531a03787caeea4c5
luwat <oluwatobioreniyi@gmail.com>
2024-09-17 17:36:40 +0100

    Add placeholder data to FlatList

--- app/(tabs)/employees.tsx
+++ app/(tabs)/employees.tsx
@@ -1,11 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { EMPLOYEES } from '@/lib/constants'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const EmployeesPage = () => {
   return (
-    <View>
-      <Text>Welcome to employees page</Text>
-    </View>
+    <SafeAreaView className='h-full w-full bg-neutral-950 p-4'>
+      <FlatList
+        data={EMPLOYEES}
+        keyExtractor={(employee) => employee.id.toString()}
+        renderItem={(employee) => (
+          <View className='text-neutral-100 my-5'>
+            <Text className='text-neutral-100'>{employee.item.firstName}</Text>
+            <Text className='text-neutral-100'>{employee.item.email}</Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   )
 }
 
