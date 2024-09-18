07fb10e8dca74d0a5a3435693de1cc3cc122d40b
luwat <oluwatobioreniyi@gmail.com>
2024-09-18 21:13:51 +0100

    Implement authentication for sign up page

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -5,34 +5,68 @@ import { useState } from "react";
 import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Link, router } from "expo-router";
+import { BASE_URL } from "@/lib/config";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const SignUp = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState("");
   const [form, setForm] = useState({
-    username: "",
     email: "",
     password: "",
   });
 
-  const submit = () => {
-    router.push('/sign-in')
-  }
+
+  const submit = async () => {
+    setIsLoading(true)
+    try {
+      const response = await fetch(`${BASE_URL}/auth/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
+      const data = await response.json();
+      if (data.error) {
+        if(Array.isArray(data.message)) {
+          setError(data.message[0])
+          return;
+        }
+        setError(data.message)
+        return;
+      }
+
+      await AsyncStorage.setItem("accessToken", data.accessToken);
+      router.push("/employees");
+
+    } catch (error: any) {
+      throw new Error(error.message)
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <SafeAreaView className="h-full bg-neutral-900">
       <ScrollView>
         <View className="w-full h-full justify-center px-4 my-6">
           <View>
-            <Text className="text-neutral-100 text-2xl text-center font-extrabold">Join Sleeky Programmers</Text>
+            <Text className="text-neutral-100 text-2xl text-center font-extrabold">
+              Join Sleeky Programmers
+            </Text>
           </View>
-          <FormField
-            title="Username"
-            placeholder="Enter username"
-            value={form.username}
-            handleChangeText={(username: string) => {
-              setForm({ ...form, username });
-            }}
-            keyboardType="default"
-          />
+          {error && (
+            <View>
+              <Text
+                className={`text-neutral-500 text-center text-lg ${
+                  error && "text-red-500"
+                }`}
+              >
+                {error}
+              </Text>
+            </View>
+          )}
           <FormField
             title="Email"
             placeholder="Enter email"
@@ -51,16 +85,24 @@ const SignUp = () => {
             }}
           />
 
-          <CustomButton title="Sign up" handlePress={submit} containerStyles="mt-7 w-full" />
+          <CustomButton
+            title={isLoading ? 'Loading' : 'Sign up'}
+            handlePress={submit}
+            containerStyles="mt-7 w-full"
+          />
           <View>
-            <Text className="text-neutral-500 text-center text-lg mt-3">Don't have an account? <Link href="/sign-in" className="text-orange-500">Sign in</Link></Text>
+            <Text className="text-neutral-500 text-center text-lg mt-3">
+              Don't have an account?{" "}
+              <Link href="/sign-in" className="text-orange-500">
+                Sign in
+              </Link>
+            </Text>
           </View>
         </View>
       </ScrollView>
-      <StatusBar style="auto"/>
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 };
 
 export default SignUp;
-
---
+++ lib/config.ts
@@ -0,0 +1,1 @@
+export const BASE_URL = "https://employee-management-api-xj3a.onrender.com"
