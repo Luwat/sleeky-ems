6521c88eea08c36ce6e1a39ecdba139a24ca141d
luwat <oluwatobioreniyi@gmail.com>
2024-09-23 18:08:34 +0100

    Create dynamic route for employee profile and edit function

---
+++ app/[id]/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const EmployeeProfilePage = () => {
+  return (
+    <View>
+      <Text>EmployeeProfilePage</Text>
+    </View>
+  )
+}
+
+export default EmployeeProfilePage
